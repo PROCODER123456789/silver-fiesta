d8de5cb67b795c57c6a8580b1667582982f2c58b
PROCODER123456789 <99879654+PROCODER123456789@users.noreply.github.com>
2022-09-04 13:31:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+img=""
+status=""
+
+
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center;
+    objectDetector=ml5.objectDetector("cocossd", modelLoaded)
+    document.getElementById("status").innerHTML="status : detecting Objects"
+}
+
+function preload(){
+    img= loadImage(Bedroom.jpeg);
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    text("bed", 45, 75);
+    noFill()
+    stroke("#FF0000")
+    rect(30, 60, 450, 350)
+
+
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    text("Table", 45, 75);
+    noFill()
+    stroke("#FF0000")
+    rect(30, 60, 450, 350)
+
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    text("Fruit basket", 45, 75);
+    noFill()
+    stroke("#FF0000")
+    rect(30, 60, 450, 350)
+
+
+}
+function modelLoaded(){
+    console.log("model is loaded !")
+    status=true;
+    objectDetector.detect(img, gotresult)
+}
+
+function gotresult(error, results){
+    if(error){
+    console.log(error);
+    }
+    console.log(results);
+}
